8ff2e4bac84b066df6264a425945503de7b49d4f
close <closethereport@gmail.com>
2022-01-30 08:10:03 +0300

    Realised methods: Create Task, Delete Task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
+
+  //Create Task
+  @Post()
+  createTask(@Body() body: any): any[] {
+    return this.taskService.createTask(body);
+  }
 
   //Get Tasks
   @Get()
@@ -15,5 +21,13 @@ export class TasksController {
   @Get(':id')
   getOneTask(@Param('id') id: string): any {
     return this.taskService.getOneTask(id);
+  }
+
+  //Delete Task 
+  @Delete(':id')
+  deleteTask(
+      @Param('id') id: string
+  ): string {
+    return this.taskService.deleteTask(id)
   }
 }
